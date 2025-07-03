88983b4ef3c1a79d2817a3778c45a9aa1a262db1
MagicalAstrogy <103271693+MagicalAstrogy@users.noreply.github.com>
2025-07-03 20:06:19 +0800

    feat: 提供事件回调来使外部脚本可以调用mvu

--- src/main.ts
+++ src/main.ts
@@ -18,4 +18,5 @@ $(window).on('unload', () => {
     eventRemoveListener(tavern_events.MESSAGE_SENT, initCheck);
     eventRemoveListener(tavern_events.MESSAGE_SENT, handleVariablesInMessage);
     eventRemoveListener(tavern_events.MESSAGE_RECEIVED, handleVariablesInMessage);
+    eventRemoveListener(variable_events.INVOKE_MVU_PROCESS, handleVariablesInCallback);
 });
