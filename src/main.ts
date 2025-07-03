0522d71fd85d14892ffe24bb0c85d17f810d7dfd
MagicalAstrogy <103271693+MagicalAstrogy@users.noreply.github.com>
2025-07-03 22:00:27 +0800

    feat: 支持重新生成变量

--- src/main.ts
+++ src/main.ts
@@ -20,3 +20,23 @@ $(window).on('unload', () => {
     eventRemoveListener(tavern_events.MESSAGE_RECEIVED, handleVariablesInMessage);
     eventRemoveListener(variable_events.INVOKE_MVU_PROCESS, handleVariablesInCallback);
 });
+
+eventOnButton('重新处理变量', async function() {
+    const last_msg = getLastMessageId();
+    if (last_msg < 0)
+        return;
+    if (SillyTavern.chat.length === 0)
+        return;
+    {
+        const last_msg_data = SillyTavern.chat[last_msg];
+        if (last_msg_data.variables?.length??0 > 0) {
+            //代表之前最后的楼层可能有变量，需要进行清理。
+            if (_.has(last_msg_data.variables![last_msg_data.swipe_id??0], 'stat_data')) {
+                //清空楼层当前 swipe 的数据。
+                await replaceVariables({}, { type: 'message', message_id: last_msg });
+            }
+        }
+    }
+    //重新处理变量
+    await handleVariablesInMessage(getLastMessageId());
+});
