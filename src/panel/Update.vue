a50d144db8052beb193d6db4f680171db39759de
StageDog <aksanajw845@gmail.com>
2026-01-19 22:25:24 +0800

    Revert "feat: 额外模型解析不再需要 1.13.4 及以上"
    This reverts commit c758acdb6f127ea057878cf396a55d8e0ca6e2b6.

--- src/panel/Update.vue
+++ src/panel/Update.vue
@@ -24,6 +24,23 @@ import Request from '@/panel/update/Request.vue';
 import Source from '@/panel/update/Source.vue';
 import update_method_help from '@/panel/update_method.md';
 import { useDataStore } from '@/store';
+import { getSillyTavernVersion } from '@/util';
+import { compare } from 'compare-versions';
+import { watch } from 'vue';
 
 const store = useDataStore();
+
+watch(
+    () => store.settings.更新方式,
+    value => {
+        if (value === '额外模型解析' && compare(getSillyTavernVersion(), '1.13.4', '<')) {
+            toastr.error(
+                "检查到酒馆版本过低，要使用'额外模型解析'请保证酒馆版本大于等于 1.13.4",
+                "[MVU]无法使用'额外模型解析'",
+                { timeOut: 5000 }
+            );
+            store.settings.更新方式 = '随AI输出';
+        }
+    }
+);
 </script>
